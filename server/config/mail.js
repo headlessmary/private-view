09a63a55620bedeae13e089ab90fbcc9b6f770cb
HeadlessMary <headlessmary2026@gmail.com>
2026-07-12 00:23:34 +0100

    Add Vercel SPA routing

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,13 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
   secure: false,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
   },
 });
 
