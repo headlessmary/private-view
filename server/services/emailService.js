7a7feee68aef94feee7b8bee0b40124edd666adc
HeadlessMary <headlessmary2026@gmail.com>
2026-07-14 01:11:56 +0100

    email update

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,39 +2,45 @@ const transporter = require("../config/mail");
 const ticketTemplate = require("./emailTemplates/ticketTemplate");
 const path = require("path");
 
-const sendTicketEmail = async (data) => {
-  try {
-    console.log("Sending email to:", data.email);
+const sendTicketEmail = async ({
+  fullName,
+  email,
+  ticketType,
+  reference,
+  qrCode,
+}) => {
+  console.log("========== SENDING EMAIL ==========");
+  console.log("Recipient:", email);
 
-    const info = await transporter.sendMail({
-      from: process.env.MAIL_FROM,
-      to: data.email,
-      subject: "Your Ticket",
-      html: ticketTemplate({
-        fullName: data.fullName,
-        ticketType: data.ticketType,
-        reference: data.reference,
-      }),
-      attachments: [
-        {
-          filename: "flyer.png",
-          path: path.join(__dirname, "../uploads/flyer.jpg"),
-          cid: "flyer",
-        },
-        {
-          filename: "ticket.png",
-          path: path.join(__dirname, "..", data.qrCode),
-          cid: "qrcode",
-        },
-      ],
-    });
+  const info = await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to: email,
+    subject: "Your Ticket - The Private View: Art & Indulgence",
 
-    console.log("EMAIL SENT");
-    console.log(info);
+    html: ticketTemplate({
+      fullName,
+      ticketType,
+      reference,
+    }),
 
-  } catch (err) {
-    console.error("EMAIL ERROR");
-    console.error(err);
-    throw err;
-  }
+    attachments: [
+      {
+        filename: "flyer.jpg",
+        path: path.join(__dirname, "../uploads/flyer.jpg"),
+        cid: "flyer",
+      },
+      {
+        filename: "ticket.png",
+        path: path.join(__dirname, "..", qrCode),
+        cid: "qrcode",
+      },
+    ],
+  });
+
+  console.log("EMAIL SENT!");
+  console.log(info);
+};
+
+module.exports = {
+  sendTicketEmail,
 };
