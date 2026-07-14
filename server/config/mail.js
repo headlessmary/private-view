c2920eaa3bed3bd47a19fb4c403e68f24bdf68f3
HeadlessMary <headlessmary2026@gmail.com>
2026-07-14 01:32:00 +0100

    latest

--- server/config/mail.js
+++ server/config/mail.js
@@ -3,11 +3,13 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT),
-  secure: false,
-
+  secure: false, // true only for port 465
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
