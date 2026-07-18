525c5d86607d2b2210321728578e6babfd8dfb8e
HeadlessMary <headlessmary2026@gmail.com>
2026-07-18 08:12:47 +0100

    google

--- client/src/components/PageTracker.jsx
+++ client/src/components/PageTracker.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "./utils/analytics";
+import ReactGA from "../utils/analytics";
 
 export default function PageTracker() {
   const location = useLocation();
 
   useEffect(() => {
+    // Don't track admin pages
+    if (location.pathname.startsWith("/admin")) return;
+
     ReactGA.send({
       hitType: "pageview",
-      page: location.pathname,
+      page: location.pathname + location.search,
     });
   }, [location]);
 
